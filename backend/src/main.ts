0af3d82f188dd50f4e31beb540eef2eb735e0cdf
Balda <mateusbalda89@gmail.com>
2024-04-30 17:04:42 -0300

    config: swagger da api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Tasks example')
+    .setDescription('The tasks API description')
+    .setVersion('0.1')
+    .addTag('tasks')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3008);
 }
 bootstrap();
