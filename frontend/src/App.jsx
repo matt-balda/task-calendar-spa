95b02de8a0843340e42882ac407509ac7bd5dd2f
Balda <mateusbalda89@gmail.com>
2024-05-01 17:03:28 -0300

    refactor: info de lista de tasks vazia

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import axios from "axios";
-import Task from '../components/Task.jsx'
-import TaskForm from '../components/TaskForm.jsx';
-import './App.css'
+import Task from "../components/Task.jsx";
+import TaskForm from "../components/TaskForm.jsx";
+import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3008/tasks/list');
+        const response = await axios.get("http://localhost:3008/tasks/list");
         setTasks(response.data);
       } catch (error) {
-        console.error('Erro ao buscar dados:', error);
+        console.error("Erro ao buscar dados:", error);
       }
     };
-  
+
     fetchData();
-  }, []);
+  }, [tasks]);
 
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Tasks list</h1>
-      <div className='tasks-list'>
-        {tasks.map((task) => (
-          <Task key={task.id} task={task} />
-        ))}
+      <div className="tasks-list">
+        {tasks.length === 0 ? (
+          <div className="no-tasks-container">
+            <p className="no-tasks-title">No tasks</p>
+            <p className="no-tasks-description">
+              There are no tasks available at the moment
+            </p>
+          </div>
+        ) : (
+          tasks.map((task) => <Task key={task.id} task={task} />)
+        )}
       </div>
       <TaskForm />
     </div>
   );
 }
 
-export default App
+export default App;
