be0a3dc221575fcbc52312f26cbcbb4fde95500c
Balda <mateusbalda89@gmail.com>
2024-05-02 05:27:38 -0300

    feat: isCompleted task verificação no banco de dados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,14 @@ import axios from "axios";
 import Task from "../components/Task.jsx";
 import TaskForm from "../components/TaskForm.jsx";
 import "./App.css";
+import Search from "../components/Search.jsx";
+import Filter from "../components/Filter.jsx";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState("Asc");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,13 +21,24 @@ function App() {
         console.error("Erro ao buscar dados:", error);
       }
     };
-
     fetchData();
-  }, [tasks]);
+  }, [tasks], [filter==="all"]);
 
+  const completeTask = async (id) => {
+    try {
+      await axios.patch(`http://localhost:3008/tasks/update/${id}`, {
+        isCompleted: !tasks.find((task) => task.id === id).isCompleted,
+      });
+    } catch (error) {
+      console.error("Error completing task: ", error);
+    }
+  }
+  
   return (
     <div className="app">
       <h1>Tasks list</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} setTasks={setTasks} />
       <div className="tasks-list">
         {tasks.length === 0 ? (
           <div className="no-tasks-container">
@@ -32,7 +48,11 @@ function App() {
             </p>
           </div>
         ) : (
-          tasks.map((task) => <Task key={task.id} task={task} />)
+          tasks
+            .filter((task) => filter === "all" ? true : filter === "complete" ? task.isCompleted : !task.isCompleted)
+            .filter((task) => task.title.toLowerCase().includes(search.toLowerCase()))
+            .sort((a, b) => sort === "Asc" ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title))
+            .map((task) => <Task key={task.id} task={task} completeTask={completeTask} />)
         )}
       </div>
       <TaskForm />
